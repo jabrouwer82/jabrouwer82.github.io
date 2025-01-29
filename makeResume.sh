c5f1e6f576c7a7e7df49241bec62e120e0643f41
Jacob Brouwer <jabrouwer82@gmail.com>
2025-01-29 16:14:54 -0600

    Convert to resume.

---
+++ makeResume.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+month=$(date +%B)
+year=$(date +%Y)
+chromium \
+   --headless \
+   --disable-gpu \
+   --run-all-compositor-stages-before-draw \
+   --no-pdf-header-footer \
+   --print-to-pdf=pdf/JacobBrouwerResume$year$month.pdf \
+   JacobBrouwerResume.html
